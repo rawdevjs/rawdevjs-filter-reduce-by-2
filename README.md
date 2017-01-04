2f4d44699c436328445898a111aa81ca3fd4f817
bergi <bergi@axolotlfarm.org>
2017-01-04 02:07:36 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rawdevjs-filter-reduce-by-2
+
+Reduces the image size by 2 (width and height).
---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict'
+
+const ReduceBy2 = require('./lib/ReduceBy2')
+
+module.exports = ReduceBy2
